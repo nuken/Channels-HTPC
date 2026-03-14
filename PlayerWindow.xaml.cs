b1a1d52480d5a57dfbdbf64113d69c3155c810ab
Bobby Vaughn <rcvaughn2@gmail.com>
2026-03-14 18:44:37 -0400

    Add files via upload

--- PlayerWindow.xaml.cs
+++ PlayerWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Threading;
+using System.Threading.Tasks;
 using LibVLCSharp.Shared;
 using System.Windows.Controls;
 
